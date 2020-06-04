9e48dee00c7dae5cc754e2835b8a9f45c070aafd
laxatzi <laxatzi@yahoo.com>
2020-06-04 17:17:52 +0300

    update hof.js and index.html files

--- hof.js
+++ hof.js
@@ -1,1 +1,17 @@
+{
+   // functions should not only be abstract but also non-altering regarding the underlying data they deal with.
 
+const reverseStr = function(str){
+   var arrFromStr = str.split("");
+   arrFromStr.reverse();
+   return arrFromStr.join("");
+}
+
+const reverseNames = reverseStr;
+
+// the above function does NOT change the value of the provided as an argument string
+var string = "Thessaloniki";
+console.log(reverseStr(string)); // "ikinolassehT"
+console.log(string); // "Thessaloniki"
+console.log(reverseNames('Maria')); // "airaM"
+}
