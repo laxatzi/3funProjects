4ff0a017a1b9ea93e63906fb6f04fe372c04e8cd
laxatzi <laxatzi@yahoo.com>
2020-06-09 17:22:14 +0300

    build a higher order function to calc avg

--- hof.js
+++ hof.js
@@ -15,6 +15,7 @@ console.log(reverseCityNames("Athens")); // "snehtA"
 
 }
 
+
 {
 'use strict';
    let numbers = [3,4,12,4];
@@ -26,12 +27,53 @@ console.log(reverseCityNames("Athens")); // "snehtA"
    console.log(sumOfNumbers); //33
 }// end block
 
+
+
 {
    'use strict';
    let numbers = [3,4,12,4];
    let sumOfNumbers = 10;
-   for(let i = 0; i<arr.length; i++{
-      sumOfNumbers += arr[i];
+   for(let i = 0; i<numbers.length; i++){
+      sumOfNumbers += numbers[i];
    }
    console.log(sumOfNumbers); // 33
 }// end block
+
+{
+   let pets = [
+      {type:'Dog', name:'Rocky', age:3},
+      {type:'Cat', name:'Lucas', age:7},
+      {type:'Cat', name:'Mark', age:6},
+      {type:'Dog', name:'Diego', age:5},
+      {type:'Dog', name:'Gloria', age:6},
+      {type:'Cat', name:'Mat', age:4},
+   ];
+
+   // check if it is dog or cats
+
+      const isDog = (el)=> el.type === 'Dog';
+      const isCat = (el)=> el.type === 'Cat';
+   // test
+      console.log(isDog(pets[1]));
+
+ const getDogs = function(arr){
+     return arr.filter(isDog)
+       };
+   console.log(getDogs(pets));
+   const getCats = function(pets){ 
+     return pets.filter(isCat);
+    };
+
+ const dogAvg = function(arr) {
+     return arr.reduce((acc, currentV)=> (acc + currentV.age ) / arr.length , 0);
+   };
+
+ const catAvg = function(arr){
+      return arr.reduce((acc, currentV)=> acc + currentV.age / arr.length, 0 );
+   };
+ const dogAvgAge = dogAvg(pets);
+ const catAvgAge = catAvg(pets)
+  
+   console.log(catAvgAge);
+
+ }//block
