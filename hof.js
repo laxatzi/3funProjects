8c120ae8941affa8c1cd355f9cb2ef6a153663d6
laxatzi <laxatzi@yahoo.com>
2020-06-09 16:03:18 +0300

    build a reduce function with native method and for loop respectively

--- hof.js
+++ hof.js
@@ -11,7 +11,27 @@ const reverseCityNames = reverseStr;
 
 var cityName = "Thessaloniki";
 console.log(reverseStr(cityName)); // "ikinolassehT"
-console.log(cityName); // "Thessaloniki"
 console.log(reverseCityNames("Athens")); // "snehtA"
 
 }
+
+{
+'use strict';
+   let numbers = [3,4,12,4];
+   // getting sum of numbers with the reduce method
+   let sumOfNumbers = numbers.reduce(function(accumulator , currentValue){
+      return accumulator + currentValue;
+   }, 10); 
+
+   console.log(sumOfNumbers); //33
+}// end block
+
+{
+   'use strict';
+   let numbers = [3,4,12,4];
+   let sumOfNumbers = 10;
+   for(let i = 0; i<arr.length; i++{
+      sumOfNumbers += arr[i];
+   }
+   console.log(sumOfNumbers); // 33
+}// end block
